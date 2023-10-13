ab2c0a4da48b5b3fee698ce4135130d8c2dee8ac
Joe McNally <joe.t.mcnally@hotmail.com>
2023-10-13 01:29:43 +0100

    Make first basic test

---
+++ vitest.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 8000,
+  },
+  build: {
+    target: "esnext",
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    testTransformMode: { web: ["/.[jt]sx?$/"] },
+  },
+});
